0055699df84ad5c1653958f94d075cac86b23695
Mitch Weaver <20451170+MitchWeaver@users.noreply.github.com>
2018-01-09 11:32:10 +0100

    pywal.sh updates

--- pywal.sh
+++ pywal.sh
@@ -2,36 +2,40 @@
 #
 # http://github.com/MitchWeaver/bin
 #
-# Autorice using pywal
+# Autorice using pywal and other tools
 #
+####################################################
 
 if [ $# -eq 0 ] ; then
-    echo "Usage: sh pywal.sh \$wallpaper_path"
+    echo "Usage: sh pywal.sh \"\$wallpaper_path\""
+    exit 1
 fi
 
 # wal's -n flag tells it to skip setting the wallpaper
 # Using feh instead forked to background speeds up the script
+wal -qni "$1"
 feh --bg-fill "$1" &
-wal -qni "$1" 
-# || { echo "wal failed - exiting." ; exit 1; }
 cat ~/.cache/wal/sequences &
 
 # copy wallpaper for it to be permanent
-rm ${HOME}/workspace/dotfiles/suckless-tools/dwm/wall
-cp "$1" $HOME/workspace/dotfiles/suckless-tools/dwm/wall &
-rm $HOME/.wall
-cp "$1" $HOME/.wall &
+rm ${HOME}/workspace/dotfiles/suckless-tools/dwm/wall -- > /dev/null 2>&1
+cp "$1" ${HOME}/workspace/dotfiles/suckless-tools/dwm/wall &
+rm ${HOME}/.wall -- > /dev/null 2>&1
+cp "$1" ${HOME}/.wall &
 
-# # Generate web browser startpage css
-spage=$HOME/workspace/dotfiles/startpage
-sass $spage/scss/style.scss $spage/style.css \
+# Generate web browser startpage css
+spage=${HOME}/workspace/dotfiles/startpage
+sass $spage/scss/style.scss $spage/style.css -- > /dev/null 2>&1 \
     || { echo "sass failed to build - exiting." ; exit 1 ; }
-rm $spage/backup.css $spage/style.css.map &
+rm $spage/backup.css $spage/style.css.map -- > /dev/null 2>&1 &
 
-# # Recomp all suckless tools
-stools=$HOME/workspace/dotfiles/suckless-tools
-sudo $HOME/bin/recomp.sh $stools/dwm/dwm $stools/st/st $stools/tabbed/tabbed
+# Recomp all suckless tools
+dir=${HOME}/workspace/dotfiles/suckless-tools
+sudo ${HOME}/bin/recomp.sh $dir/dwm/dwm $dir/st/st $dir/tabbed/tabbed -- > /dev/null 2>&1 
 
-pkill -9 bar lemonbar compton
-bar &
-compton &
+# kill running procs
+pkill -9 bar lemonbar compton bash sleep -- > /dev/null 2>&1 
+
+# relaunch 
+nohup bar -- > /dev/null 2>&1 &
+nohup compton -- > /dev/null 2>&1 &
