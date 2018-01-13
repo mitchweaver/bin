6a3f9237691063bf5d11502767f9a9c207e95672
Mitch Weaver <20451170+MitchWeaver@users.noreply.github.com>
2018-01-13 19:45:24 +0100

    add miniclip
    edit prev commit
    
    fixup miniclip

--- dmenu.sh
+++ dmenu.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # http://github.com/MitchWeaver/bin
 
@@ -7,6 +7,15 @@ if [ $(pgrep dmenu) ] ; then
 fi
 
 # Import the colors
-. "${HOME}/.cache/wal/colors.sh"
+. ${HOME}/.cache/wal/colors.sh
 
-dmenu -f -nb "$color0" -nf "$color15" -sb "$color2" -sf "$color15" "$@"
+DIM=$(xrandr --current | grep \* | awk '{print $1}')
+sw=$(echo "$DIM" | sed 's/x.*//') # screen width
+sh=$(echo "$DIM" | sed 's/^[^x]*x//') # screen height
+
+w=$(echo "$sw / 1.5" | bc) # width
+x=$(echo "$sw / 2 - $w / 2" | bc) # x-offset
+y=$(echo "$sh / 6" | bc) # y-offset
+h=$(echo "$sh / 40" | bc) # height
+
+dmenu -l $h -nb $color0 -nf $color15 -sb $color2 -sf $color15 -x $x -y $y -wi $w "$@"
