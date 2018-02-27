700e62230f2009d045d9b88be267737648976c3a
Mitch Weaver <20451170+MitchWeaver@users.noreply.github.com>
2018-02-27 01:59:19 +0100

    update miniclip

--- plumb.sh
+++ plumb.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env dash
 
+# echo "$@" >> /home/mitch/test.txt
 
-echo "$@" >> /home/mitch/test.txt
+echo "$(sselp)" >> /home/mitch/test.txt
+
+pattern="$(sselp)"
+[ -z "$pattern" ] &&
+    exit
+
+patternbase=${pattern%%:*} 
