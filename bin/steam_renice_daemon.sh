dd8280ae0c3c3ecbe273b22cb7e869c74f411749
wvr <mitch@wvr.sh>
2024-09-15 11:12:06 -0500

    add steam renice daemon

---
+++ bin/steam_renice_daemon.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# shitty script to keep steam games running at highest priority
+#
+# -20 for nice (highest)
+# 1 for ionice (real time)
+#
+# https://github.com/mitchweaver/bin
+#
+
+if [ "$(id -u)" -ne 0 ] ; then
+    >&2 echo "Must be run as root."
+    exit 1
+fi
+
+INTERVAL=$(( 60 * 5 ))
+
+while : ; do
+    pgrep -f steam | \
+    while read -r pid ; do
+        renice -n -20 -p "$pid" && echo "Successfully changed process '$pid' nice to -20"
+        ionice -c 1 -p "$pid" && echo "Successfully changed process '$pid' ionice to Real Time"
+    done
+    sleep $INTERVAL
+done
+
