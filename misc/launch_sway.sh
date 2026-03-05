889d8b9ac164a85c99b18b4b0bbba68190657da8
wvr <mitch@wvr.sh>
2026-03-05 03:06:00 -0600

    bright: add brightnessctl support (alpine)

--- misc/launch_sway.sh
+++ misc/launch_sway.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 export _JAVA_AWT_WM_NONREPARENTING=1
+export MOZ_ENABLE_WAYLAND=1
+export QT_QPA_PLATFORM=wayland
+export SDL_VIDEODRIVER=wayland
+
 export XCURSOR_SIZE=24
 
 export XDG_RUNTIME_DIR="/tmp/.$USER-xdg_runtime_dir"
@@ -11,12 +15,12 @@ fi
 mkdir -p "${XDG_RUNTIME_DIR}"
 chmod 0700 "${XDG_RUNTIME_DIR}"
 
-export XDG_SESSION_TYPE=sway
+export XDG_SESSION_TYPE=wayland
 export XDG_SESSION_DESKTOP=sway
+export XDG_CURRENT_DESKTOP=sway
 
 ############export WLR_NO_HARDWARE_CURSOR=1
 ############export WLR_RENDERER_ALLOW_SOFTWARE=1
-
 ##########export WLR_DRM_DEVICES=/dev/dri/card0
 
 mkdir -p ~/.cache
